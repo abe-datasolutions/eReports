930f392018e4cc7077241d842501acd3fa825b72
James Dave Navor <james.navor@abedatasolutions.com.ph>
2025-02-17 13:24:05 +0800

    Updated compileSdk to 32 and targetSdk to 32. Set maxSdkVersion for WRITE_EXTERNAL_STORAGE to 29. Set maxSdkVersion for READ_EXTERNAL_STORAGE to 28. Set JVM target to 17. Set sourceCompatibility to Java 17. Set targetCompatibility to Java 17.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 }
 android {
     namespace = "com.abclab.abcereports"
-    compileSdk = 28
+    compileSdk = 32
     useLibrary("org.apache.http.legacy")
     defaultConfig {
         applicationId = "com.abclab.abcereports"
         minSdk = 21
         //noinspection EditedTargetSdkVersion,ExpiredTargetSdkVersion
-        targetSdk = 28
+        targetSdk = 32
 
         buildConfigField(
             type = "int",
@@ -32,11 +32,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         buildConfig = true
