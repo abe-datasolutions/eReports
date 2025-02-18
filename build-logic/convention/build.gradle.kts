cd253ab153ca26c7f1da7e998a53f49db2097cf3
James Dave Navor <james.navor@abedatasolutions.com.ph>
2025-02-18 09:50:57 +0800

    Setup Build Logic Included Build Module

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `kotlin-dsl`
+}
+group = "com.abedatasolutions.ereport.build.logic"
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.tools.build.gradle)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradle.plugin)
+    compileOnly(libs.compose.compiler.gradle.plugin)
+    compileOnly(libs.ksp.gradle.plugin)
+}
+
+gradlePlugin {
+    plugins {
+//        register("androidApplication"){
+//            id = "ecourier.android.application"
+//            implementationClass = "AndroidApplicationConventionPlugin"
+//        }
+    }
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.compilerOptions {
+    languageVersion = KotlinVersion.KOTLIN_1_9
+}
