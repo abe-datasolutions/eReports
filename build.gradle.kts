044dbc33b428a3c5174bf42cc9641ac9b2b996e1
James Dave Navor <james.navor@abedatasolutions.com.ph>
2025-02-26 14:56:16 +0800

    Setup Models Module Added Room and Coroutines Dependencies Optimized Project Plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,4 +16,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.androidx.room) apply false
 }
---
+++ core/models/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    alias(libs.plugins.ereports.jvm.library)
+    alias(libs.plugins.ksp)
+    id(libs.plugins.kotlin.plugin.serialization.get().pluginId)
+}
+dependencies {
+    implementation(libs.kotlinx.serialization.json)
+    api(libs.kotlinx.datetime)
+    implementation(libs.ktor.client.core)
+}
